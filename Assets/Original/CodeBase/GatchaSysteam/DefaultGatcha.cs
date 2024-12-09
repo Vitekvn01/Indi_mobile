e27482aa36cdafc3b162f77f8a4141c85c105f89
BoldElf <denis.den.kovalenko11@mail.ru>
2024-12-09 13:03:43 +0300

    Settings

--- Assets/Original/CodeBase/GatchaSysteam/DefaultGatcha.cs
+++ Assets/Original/CodeBase/GatchaSysteam/DefaultGatcha.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using Zenject;
 
 public class DefaultGatcha : GatchaGlobal
 {
-
+    
 }
